46cdd753833d7ff8dc91aa9a38f82b58d6c4d6e9
Min-Hyeok <alsgur0008@naver.com>
2021-05-26 13:09:59 +0900

    typeorm 추가, db정보 설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 // import { ServeStaticModule } from '@nestjs/serve-static';
 // import { join } from 'path';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "ec2-52-87-107-83.compute-1.amazonaws.com",
+      "port": 5432,
+      "username": "xmxmphxbhkuzbr",
+      "password": "01b1b3ae06491502d486ce81c4963fafbbb0593c053c951941cb87d0f0ee19a3",
+      "database": "dfa36ucofbor0a",
+      "extra": {
+          "ssl": {
+              "rejectUnauthorized": false
+          }
+      },
+      "entities": [],
+    })
     // ServeStaticModule.forRoot({
     //   rootPath: join(__dirname, '..', 'static'),
     //   renderPath: join(__dirname, '..', 'static'),
